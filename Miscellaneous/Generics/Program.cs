c12c162cae15a5d542cea8ef22ddce6a6a69806b
Krzysztof Bialecki <chrisbialecki@yahoo.co.uk>
2016-07-27 18:39:19 +0100

    go

--- Miscellaneous/Generics/Program.cs
+++ Miscellaneous/Generics/Program.cs
@@ -7,14 +7,48 @@ using System.Threading.Tasks;
 namespace Generics
 {
 
+    public class MyGenericArray<T>
+    {
+        //declare generic private array
+        private T[] array;
 
+        //construcor + instance of array
+        public MyGenericArray(int size)
+        {
+            array = new T[size + 1];
+        }
 
+        //method generic getItem
+        public T getItem(int index)
+        {
+            return array[index];
+        }
+
+        //method setItem
+        public void setItem(int index, T value)
+        {
+            array[index] = value;
+        }
+    }
 
 
     class Program
     {
         static void Main(string[] args)
         {
+            MyGenericArray<int> intArray = new MyGenericArray<int>(5);
+
+            for (int c = 0; c < 5; c++)
+            {
+                intArray.setItem(c, c * 5);
+            }
+
+            for (int c = 0; c < 5; c++)
+            {
+                Console.Write(intArray.getItem(c) + " ");
+            }
+
+            Console.WriteLine();
         }
     }
 }
