048567c8afb6bcc7959f46263d6c208beffdd4db
Krzysztof Bialecki <chrisbialecki@yahoo.co.uk>
2016-07-28 17:42:48 +0100

    go

--- Miscellaneous/Pass_Argument_By_Ref/Program.cs
+++ Miscellaneous/Pass_Argument_By_Ref/Program.cs
@@ -8,8 +8,34 @@ namespace Pass_Argument_By_Ref
 {
     class Program
     {
+        int yOut = 5;
+
+        static void Method(ref int i)
+        {
+            i = i + 44;
+        }
+
+        public void MethodOut(out int y)
+        {           
+
+            y = yOut * 2;                 
+
+        }
+
+
         static void Main(string[] args)
         {
+            int valRef = 1;
+            Method(ref valRef);
+            Console.WriteLine(valRef);
+
+            int valOut;
+            Program p = new Program();
+            p.MethodOut(out valOut);
+            Console.WriteLine(valOut);
+
+            Console.ReadLine();
         }
+        
     }
 }
