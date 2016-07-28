797c018864c99b9a05fe74f1471a33dec2b47d31
Krzysztof Bialecki <chrisbialecki@yahoo.co.uk>
2016-07-28 18:14:45 +0100

    go

---
+++ Miscellaneous/Lambda/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda
+{
+    class Program
+    {
+        // parameters: in, in, out
+        public static Func<string, string, int> func1 = (x, y) => x.Length + y.Length;
+
+
+        static void Main(string[] args)
+        {
+            var i = func1("home", "us");
+            Console.WriteLine(i);
+            
+            Console.ReadLine();
+        }
+    }
+}
