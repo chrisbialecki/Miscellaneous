0784b1e617da36f31ba2ae216516e0d32420b133
chrisbialecki <chrisbialecki@yahoo.co.uk>
2016-07-29 18:03:05 +0100

    go

--- Miscellaneous/Lambda/Program.cs
+++ Miscellaneous/Lambda/Program.cs
@@ -8,16 +8,23 @@ namespace Lambda
 {
     class Program
     {
-        // parameters: in, in, out
-        public static Func<string, string, int> func1 = (x, y) => x.Length + y.Length;
+        //// parameters: in, in, out
+        //public static Func<string, string, int> func1 = (x, y) => x.Length + y.Length;
+      public string x = "home";
 
+        
 
         static void Main(string[] args)
         {
-            var i = func1("home", "us");
-            Console.WriteLine(i);
+        //    var i = func1("home", "us");
+        //    Console.WriteLine(i);
             
+        //    Console.ReadLine();
+            Program p = new Program();
+           
+            Console.WriteLine(p.x == "home" ? "true" : "false");
             Console.ReadLine();
+
         }
     }
 }
